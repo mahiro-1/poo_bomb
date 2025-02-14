27c871eaccd2500f15d1b1b882ae1e72e404fc51
shunto-ishiguro <b1024017@fun.ac.jp>
2025-02-14 17:14:09 +0900

    change pin

--- poo_bomb/Assets/Scripts/UnkoDartsPresenter.cs
+++ poo_bomb/Assets/Scripts/UnkoDartsPresenter.cs
@@ -5,6 +5,17 @@ using UnityEngine.UI;
 public class UnkoDartsPresenter : MonoBehaviour
 {
     [SerializeField] Button button;
+    [SerializeField] private GameObject Board;
+
+    [SerializeField] private GameObject Unko;
+
+
+
+    private float MoveSpeed = 3.0f;
+    void FixedUpdate()
+    {
+        Board.transform.position = new Vector3(Mathf.Sin(Time.time) * MoveSpeed, -4.3f, 0);
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -19,6 +30,6 @@ public class UnkoDartsPresenter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
