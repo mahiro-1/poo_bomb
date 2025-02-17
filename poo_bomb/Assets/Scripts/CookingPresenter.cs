5f2ddf409264254e482ceec04476f2b35bb7d291
bancho <riceomeletomuraisu@gmail.com>
2025-02-17 15:40:05 +0900

    音ゲー制作の途中経過

--- poo_bomb/Assets/Scripts/CookingPresenter.cs
+++ poo_bomb/Assets/Scripts/CookingPresenter.cs
@@ -1,13 +1,32 @@
+using System;
 using UnityEngine;
 
 public class CookingPresenter : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Serializable] public class InputJson
     {
-        
+        public Notes[] notes;
+        public int BPM;
     }
-
+    [Serializable] public class Notes
+    {
+        public int num;
+        public int block;
+        public int LPB;
+    }
+    private int[] scoreNum;
+    private int[] scoreBlock;
+    private int BPM;
+    private int LPB;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        MusicReading();
+    }
+    void MusicReading()
+    {
+        string inputString = Resources.Load<TextAsset>("testbgm").ToString();
+    }
     // Update is called once per frame
     void Update()
     {
