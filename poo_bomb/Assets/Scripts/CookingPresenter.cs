c0918aa7f43d7b305d8ab75572576514fb80e4ff
bancho <riceomeletomuraisu@gmail.com>
2025-02-19 12:54:59 +0900

    タップ判定の追加

--- poo_bomb/Assets/Scripts/CookingPresenter.cs
+++ poo_bomb/Assets/Scripts/CookingPresenter.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 using UnityEngine.Video;
 using UniRx;
 using UniRx.Triggers;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
 
 public class CookingPresenter : MonoBehaviour
 {
@@ -30,19 +32,19 @@ public class CookingPresenter : MonoBehaviour
 
     private int gameCount;
     private List<List<int>> notesAddress;
-    private Queue<GameObject> nowNotes;
+    private List<GameObject> nowNotes;
     [SerializeField] GameObject Note;
-    [SerializeField] GameObject DestroyArea;
+    [SerializeField] GameObject TouchBar;
     private float moveSpeed = 0.18f;
     private float timeOut = 0.03f;
-    private float[] blockPos = {-3f,-1.5f,0f,1.5f,3f};
+    private float[] blockPos = {-2f,-1f,0f,1f,2f};
+    private float[] touchBarPosX = {-2.5f, -1.5f, -0.5f, 0.5f, 1.5f, 2.5f};
+    private float touchBarPosYMax =-3.0f;
+    private float touchBarPosYMin =-4.9f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        DestroyArea.OnTriggerEnter2DAsObservable().Subscribe(x => {
-            Destroy(x.gameObject);
-            nowNotes.Dequeue();
-        });
+        //TouchBar.On
         
         MusicReading();
         //音ゲーを開始する関数（後でボタンを押されたときに始めるとかにするで）
@@ -78,35 +80,71 @@ public class CookingPresenter : MonoBehaviour
 
     void GameStart(){
         gameCount = 0;
-        nowNotes = new Queue<GameObject>();
+        nowNotes = new List<GameObject>();
         //ノーツを生成する関数を繰り返し呼び出すやつ。繰り返す秒数は音楽のLPBに合わせる
-        InvokeRepeating("CreateNotes", 0f, 0.05f);
         StartCoroutine(MoveNotes());
     }
 
-    //ロードしたデータをもとにノーツを生成
-    void CreateNotes(){
-        if(notesAddress.Count <= gameCount) return;
-        foreach(int block in notesAddress[gameCount]){
-            GameObject nnote = Instantiate(Note, new Vector3(blockPos[block],1.2f,0f), Quaternion.Euler(0,0,90));
-            nowNotes.Enqueue(nnote);
-
-        }
-        gameCount++;
-    }
     IEnumerator MoveNotes()
     {
         while (true)
         {
-            int destroyCount = 0;
-            //timeOutの時間でループする
+            //ノーツ生成処理
+            if(notesAddress.Count > gameCount){
+                foreach(int block in notesAddress[gameCount]){
+                    GameObject nnote = Instantiate(Note, new Vector3(blockPos[block],1.2f,0f), Quaternion.Euler(0,0,90));
+                    nowNotes.Add(nnote);
+                }
+            }
+            gameCount++;
+
+            //ノーツ移動処理＆破壊処理
+            List<GameObject> destroyNotes = new List<GameObject>();
             foreach(var x in nowNotes){
                 x.transform.position += new Vector3(0f, -moveSpeed, 0f);
                 if(x.transform.position.y < -5.0f){
-                    destroyCount++;
+                    destroyNotes.Add(x);
                 }
             }
-            for(int i = 0; i < destroyCount; i++) Destroy(nowNotes.Dequeue());
+            foreach(var x in destroyNotes){
+                nowNotes.Remove(x);
+                Destroy(x);
+            }
+
+            //画面タッチ判定
+            int touchCount = Input.touchCount;
+            for(int i = 0; i < touchCount; i++){
+                Touch touch = Input.GetTouch(i);
+                if(touch.phase == TouchPhase.Began){
+                    Vector2 touchpos = touch.position;
+                    Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(touchpos.x, touchpos.y, 1.0f)); 
+                    //Debug.Log("tap!!" + "positon:" + pos.x + "," + pos.y);
+                    if(pos.y < touchBarPosYMin || touchBarPosYMax < pos.y)continue;
+                    for(int j = 0; j < 5; j++){
+                        if(touchBarPosX[j] <= pos.x && pos.x < touchBarPosX[j+1]){
+                            //Debug.Log("touch Bar" + blockPos[j]);
+                            //jがブロックの位置の配列の場所を表す
+                            GameObject CandidateNote = new GameObject();
+                            CandidateNote.transform.position = new Vector3(0f,10f,0f);
+                            foreach(var x in nowNotes){
+                                if(x.transform.position.x == blockPos[j]){
+                                    if(CandidateNote.transform.position.y > x.transform.position.y){
+                                        CandidateNote = x;
+                                    }
+                                }
+                            }
+                            
+                            //ノーツ判定＆ノーツ破壊
+                            if(CandidateNote.transform.position.y < -3f){
+                                Debug.Log("Good!");
+                            }
+                            Destroy(CandidateNote);
+                            nowNotes.Remove(CandidateNote);
+                        }
+                    }
+                }
+            }
+            //for(int i = 0; i < destroyCount; i++) Destroy(nowNotes.Dequeue());
             yield return new WaitForSeconds(timeOut);
         }
     }
