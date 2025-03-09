14b22b05b56f4ce7e4c106ddf81cac0117d5a68f
bancho <riceomeletomuraisu@gmail.com>
2025-03-09 16:40:18 +0900

    動きなおした太郎丸三代目船長就任

--- poo_bomb/Assets/Scripts/DashPresenter.cs
+++ poo_bomb/Assets/Scripts/DashPresenter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
+using TMPro;
 using UniRx;
 using UniRx.Triggers;
 using Unity.VisualScripting;
@@ -10,6 +11,7 @@ public class DashPresenter : MonoBehaviour
 {
     [SerializeField] private GameObject playerCamera;
     [SerializeField] private GameObject player;
+    [SerializeField] private TextMeshProUGUI coinText;
     //[SerializeField] private GameObject moveSpace;
     private int routeFlag = 0;
     [SerializeField] private float moveSpeed = 20f;
@@ -21,29 +23,42 @@ public class DashPresenter : MonoBehaviour
     private int limit_flag = 0;
     private int countCoin = 0;
     private bool isMoveChange = true;
+    private bool isSwipe = false;
 
-    enum TurnDirection {
+    enum TurnDirection
+    {
         Right,
         Left
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        player.OnTriggerEnterAsObservable().Where(x => x.gameObject.tag == "coin").Subscribe(x =>
+        player.OnTriggerEnterAsObservable().Subscribe(x =>
         {
-            countCoin++;
-            Debug.Log("coin get");
-            Destroy(x.gameObject);
+            if (x.gameObject.tag == "coin")
+            {
+                countCoin++;
+                Destroy(x.gameObject);
+            }
+            else if (x.gameObject.tag == "unko")
+            {
+                countCoin -= 3;
+                if (countCoin < 0) countCoin = 0;
+                Destroy(x.gameObject);
+            }
         }).AddTo(this);
     }
-    private IEnumerator Turn(TurnDirection t){
+    private IEnumerator Turn(TurnDirection t)
+    {
         Vector3 turnVector = new Vector3();
         Vector3 maxTurn = new Vector3();
-        if(t == TurnDirection.Right){
+        if (t == TurnDirection.Right)
+        {
             turnVector = new Vector3(0f, turnSpeed, 0f);
             maxTurn = new Vector3(0f, 0f, 0f);
         }
-        else if(t == TurnDirection.Left){
+        else if (t == TurnDirection.Left)
+        {
             turnVector = new Vector3(0f, -turnSpeed, 0f);
             maxTurn = new Vector3(0f, 270f, 0f);
         }
@@ -62,42 +77,11 @@ public class DashPresenter : MonoBehaviour
             }
         }
     }
-    /*
-    private IEnumerator TurnLeft()
-    {
-        while (true)
-        {
-            moveSpace.transform.eulerAngles += new Vector3(0f, -turnSpeed, 0f);
-            playerCamera.transform.eulerAngles += new Vector3(0f, -turnSpeed, 0f);
-            yield return new WaitForFixedUpdate();
-            if (moveSpace.transform.eulerAngles.y <= 270f)
-            {
-                moveSpace.transform.eulerAngles = new Vector3(0f, -90f, 0f);
-                playerCamera.transform.eulerAngles = new Vector3(0f, -90f, 0f);
-                yield break;
-            }
-        }
-    }
-    private IEnumerator TurnRight()
-    {
-        while (true)
-        {
-            moveSpace.transform.eulerAngles += new Vector3(0f, turnSpeed, 0f);
-            //playerCamera.transform.eulerAngles += new Vector3(0f, turnSpeed, 0f);
-            if (moveSpace.transform.eulerAngles.y <= turnSpeed)
-            {
-                moveSpace.transform.eulerAngles = new Vector3(0f, 0f, 0f);
-                //playerCamera.transform.eulerAngles = new Vector3(0f,0f,0f);
-                yield break;
-            }
-            yield return new WaitForFixedUpdate();
-        }
-    }
-    */
     void FixedUpdate()
     {
-        DetectSwipe();
-        if(isMoveChange){
+        ProcessSwipe();
+        if (isMoveChange)
+        {
             Vector3 moveVector = new Vector3(0f, 0f, 0f);
             if (routeFlag == 0) moveVector = new Vector3(0f, 0f, moveSpeed);
             else if (routeFlag == 1) moveVector = new Vector3(-moveSpeed, 0f, 0f);
@@ -137,7 +121,12 @@ public class DashPresenter : MonoBehaviour
     }
     void LateUpdate()
     {
-        
+
+    }
+    void Update()
+    {
+        coinText.text = "Coin :" + countCoin.ToString();
+        DetectSwipe();
     }
 
     private void DetectSwipe()
@@ -149,41 +138,46 @@ public class DashPresenter : MonoBehaviour
         if (Input.GetMouseButtonUp(0)) // タッチ終了
         {
             endTouchPosition = Input.mousePosition;
-            ProcessSwipe();
+            isSwipe = true;
         }
     }
 
     private void ProcessSwipe()
     {
-        Vector3 swipeVector = endTouchPosition - startTouchPosition;
-
-        if (swipeVector.magnitude < swipeThreshold) return; // 閾値以下なら無視
-
-        Vector3 newPosition = player.transform.position;
-
-        if (Mathf.Abs(swipeVector.x) > Mathf.Abs(swipeVector.y))
+        if (isSwipe)
         {
-            if (swipeVector.x > 0 && limit_flag < 1) // 右スワイプ
+            Vector3 swipeVector = endTouchPosition - startTouchPosition;
+
+            if (swipeVector.magnitude < swipeThreshold) return; // 閾値以下なら無視
+
+            Vector3 newPosition = player.transform.position;
+
+            if (Mathf.Abs(swipeVector.x) > Mathf.Abs(swipeVector.y))
             {
-                if (routeFlag == 0) newPosition.x += 5f;
-                else if (routeFlag == 1) newPosition.z += 5f;
-                else if (routeFlag == 2) newPosition.x += 5f;
-                limit_flag += 1;
+                if (swipeVector.x > 0 && limit_flag < 1) // 右スワイプ
+                {
+                    if (routeFlag == 0) newPosition.x += 5f;
+                    else if (routeFlag == 1) newPosition.z += 5f;
+                    else if (routeFlag == 2) newPosition.x += 5f;
+                    limit_flag += 1;
+                }
+                if (swipeVector.x < 0 && limit_flag > -1)// 左スワイプ
+                {
+                    if (routeFlag == 0) newPosition.x -= 5f;
+                    else if (routeFlag == 1) newPosition.z -= 5f;
+                    else if (routeFlag == 2) newPosition.x -= 5f;
+                    limit_flag -= 1;
+                }
             }
-            if (swipeVector.x < 0 && limit_flag > -1)// 左スワイプ
+            else
             {
-                if (routeFlag == 0) newPosition.x -= 5f;
-                else if (routeFlag == 1) newPosition.z -= 5f;
-                else if (routeFlag == 2) newPosition.x -= 5f;
-                limit_flag -= 1;
+                return;
             }
-        }
-        else
-        {
-            return;
+
+            player.transform.position = newPosition;
+            isSwipe = false;
         }
 
-        player.transform.position = newPosition;
     }
 }
 
