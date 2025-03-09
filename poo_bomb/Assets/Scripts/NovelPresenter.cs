79f5faaef8e7d62333a67444f637a76e24256e29
shunto-ishiguro <b1024017@fun.ac.jp>
2025-03-09 16:39:40 +0900

    Sound_Addtion1

--- poo_bomb/Assets/Scripts/NovelPresenter.cs
+++ poo_bomb/Assets/Scripts/NovelPresenter.cs
@@ -12,6 +12,7 @@ public class Presenter : MonoBehaviour
     [SerializeField] private Image characterImage;
     [SerializeField] private TextMeshProUGUI storyText;
     [SerializeField] private TextMeshProUGUI characterName;
+    [SerializeField] private SoundPlayer soundPlayer;
     private Boolean endflag = false;
     public int textIndex { get; private set; }
     private int storyIndex;
@@ -25,20 +26,29 @@ public class Presenter : MonoBehaviour
         Observable.EveryUpdate()
             .Where(_ => Input.GetMouseButtonDown(0) && endflag == false)
             .ThrottleFirst(TimeSpan.FromMilliseconds(200)) // 0.2秒以内の連続クリックを無視し、意図しない二重クリックを防ぐ
-            .Subscribe(_ => OnNextText())
+            .Subscribe(_ =>
+            {
+                soundPlayer.PlaySound();
+                Observable.Timer(System.TimeSpan.FromSeconds(0.5)) // 0.5秒待つ
+            .Subscribe(__ => OnNextText());
+            })
             .AddTo(this);
     }
     private void OnNextText()
     {
+        endflag = true;
         textIndex++;
         storyText.text = "";
         if (textIndex >= storyDatas[storyIndex].stories.Count)
         {
             SceneLoader.NextScene();
         }
-        else{
+        else
+        {
             SetStoryElement(storyIndex, textIndex);
         }
+        Observable.Timer(System.TimeSpan.FromSeconds(2.0)) // 2.0秒待つ
+        .Subscribe(__ => endflag = false);
     }
 
     private void SetStoryElement(int _storyIndex, int _textIndex)
